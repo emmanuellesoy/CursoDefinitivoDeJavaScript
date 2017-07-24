4dc450aac16395fa3292e657e9876582ad24e80e
shannonbit <emmanuelle.laguna@gmail.com>
2017-07-24 18:52:04 -0500

    Se agrego express y se configuro un servidor

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express')
+
+var app = express()
+
+app.get('/', function(req, res){
+   res.send('¡Hola mundo!')
+})
+
+app.listen(3000, function(err){
+   if(err) return console.log('Hubo un eroor'), proccess.exit(1)
+
+   console.log('Escuchando en el puerto 3000');
+})
